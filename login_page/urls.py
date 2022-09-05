f79ad4f319aa25e08ea6c3757560a48a2dc71a9a
Oloyede Adeosun <adesleem01@gmail.com>
2022-09-05 22:08:24 +0100

    commit_2

--- login_page/urls.py
+++ login_page/urls.py
@@ -5,7 +5,7 @@ from .views import HomePage, Register, Login
 urlpatterns = [
     path('home/', HomePage, name='home-page'),
     path('register/', Register, name='register-page'),
-    path('login/', Login, name='logi-page')
+    path('login/', Login, name='login-page')
 
     
 ]
--- login_page/views.py
+++ login_page/views.py
@@ -1,5 +1,5 @@
 import email
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 
 
@@ -14,18 +14,19 @@ def Register(request):
     if request.method == 'POST':
         fname = request.POST.get('fname')
         lname = request.POST.get('sname')
-        user_name = request.POST.get('uname')
+        name = request.POST.get('uname')
         email = request.POST.get('email')
         password = request.POST.get('pass')
 
 
 
-        new_user = User.objects.create_user(user_name, email, password)
+        new_user = User.objects.create_user(name, email, password)
         new_user.first_name = fname
         new_user.last_name = lname
 
 
         new_user.save()
+        return redirect('login-page')
 
     return render(request, 'login_page/register.html', {})
 
